b622124938debdcd2e14a12c5d619a5e86c4794a
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2020-07-09 00:38:33 +0200

    Stores command

--- internal/cli/beers.go
+++ internal/cli/beers.go
@@ -6,8 +6,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// CobraFn function definion of run cobra command
-type CobraFn func(cmd *cobra.Command, args []string)
+// CobraBeersFn function definion of run cobra command
+type CobraBeersFn func(cmd *cobra.Command, args []string)
 
 var beers = map[string]string{
 	"01D9X58E7NPXX5MVCR9QN794CH": "Mad Jack Mixer",
@@ -15,7 +15,7 @@ var beers = map[string]string{
 	"01D9X5CVS1M9VR5ZD627XDF6ND": "Belgian Moon",
 }
 
-const idFlag = "id"
+const idBeersFlag = "id"
 
 // InitBeersCmd initialize beers command
 func InitBeersCmd() *cobra.Command {
@@ -25,14 +25,14 @@ func InitBeersCmd() *cobra.Command {
 		Run:   runBeersFn(),
 	}
 
-	beersCmd.Flags().StringP(idFlag, "i", "", "id of the beer")
+	beersCmd.Flags().StringP(idBeersFlag, "i", "", "id of the beer")
 
 	return beersCmd
 }
 
-func runBeersFn() CobraFn {
+func runBeersFn() CobraBeersFn {
 	return func(cmd *cobra.Command, args []string) {
-		id, _ := cmd.Flags().GetString(idFlag)
+		id, _ := cmd.Flags().GetString(idBeersFlag)
 
 		if id != "" {
 			fmt.Println(beers[id])
