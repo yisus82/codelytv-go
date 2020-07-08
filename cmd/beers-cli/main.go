0ff4f91cc6f4ef5ff5d4e76c4f68ef6e57832dcf
Jesús Ángel Pérez-Roca Fernández <djalma8@gmail.com>
2020-07-09 00:36:07 +0200

    Beers command

---
+++ cmd/beers-cli/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/yisus82/codelytv-go/internal/cli"
+)
+
+func main() {
+	rootCmd := &cobra.Command{Use: "beers-cli"}
+	rootCmd.AddCommand(cli.InitBeersCmd())
+	rootCmd.Execute()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yisus82/codelytv-go
+
+go 1.14
+
+require github.com/spf13/cobra v1.0.0
